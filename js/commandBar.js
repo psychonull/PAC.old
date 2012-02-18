774f40eef29f132c4ee1f3ba386da815c0e0fa53
matiasarriola <matias.arriola@gmail.com>
2012-02-18 17:42:22 -0300

    some test cases added. events and commandBar are now part of PAC :D

---
+++ js/commandBar.js
@@ -0,0 +1,4 @@
+/*
+ * will be the bar in the bottom will handle actions, char's inventory, 
+ * and maybe text outputs after actions are triggered
+ */
---
+++ js/events.js
@@ -0,0 +1,4 @@
+/**
+ * will suscribe to canvas mouse/kb events and trigger 
+ * actions depending on events.
+ */
